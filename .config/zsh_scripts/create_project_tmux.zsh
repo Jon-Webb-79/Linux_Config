10f6530d21a1f197f2d48b3d081a3013cb292c11
Jon Webb <webbja123@gmail.com>
2024-03-15 11:46:02 -0600

    Updated create_project_tmux.zsh file

--- .config/zsh_scripts/create_project_tmux.zsh
+++ .config/zsh_scripts/create_project_tmux.zsh
@@ -17,87 +17,84 @@
 
 SESSION=$2
 language=$1
-# --------------------------------------------------------------------------------
-# Error checking
 
-# Verify that parameters were properly passed in the command line
-if [ -z $SESSION ] ; then
-	echo "FATAL ERROR: SESSION not set"
-	exit 1
+# Error checking
+if [ -z "$SESSION" ] ; then
+    echo "FATAL ERROR: SESSION not set"
+    exit 1
 fi
-if [ -z $language ] ; then
-	echo "FATAL ERROR: language not set"
-	exit 1
+if [ -z "$language" ] ; then
+    echo "FATAL ERROR: language not set"
+    exit 1
 fi
 
 # Verify that the entry is acceptable
-if [ $language != "Python" ] && [ $language != "C" ] && [ $language != "C++" ] && \
-    [ $language != "Arduino" ]; then
-	echo 'FATAL ERROR: Language must be Python, C, C++, or Arduino'
+if [ "$language" != "Python" ] && [ "$language" != "C" ] && [ "$language" != "C++" ] && \
+    [ "$language" != "Arduino" ]; then
+    echo 'FATAL ERROR: Language must be Python, C, C++, or Arduino'
+    exit 1
 fi
-# ================================================================================
-# Set the path length and laiases
+
+# Set the path length and aliases
 
 # Path to project
-dir_path=$HOME'/Code_Dev/'$language'/'$SESSION
+dir_path="$HOME/Code_Dev/$language/$SESSION"
 
 # Check to ensure directory exists
 if [ ! -d "$dir_path" ]; then
-    echo "FATAL ERROR: $file_path does not exist" && exit
+    echo "FATAL ERROR: $dir_path does not exist" && exit 1
 else
-    cd $dir_path'/'$SESSION
+    cd "$dir_path/$SESSION" || exit 1
 fi
-# ================================================================================ 
+
 # Develop list of open tmux sessions for comparison
+SESSIONEXISTS=$(tmux list-sessions | grep "$SESSION")
 
-
-SESSIONEXISTS=$(tmux list-sessions | grep $SESSION)
-# ================================================================================ 
 # If tmux session does not already exist, create one and attach it
-
-if [ "$SESSIONEXISTS" = "" ]; then
+if [ -z "$SESSIONEXISTS" ]; then
     # Start new session with Name
-    tmux new-session -d -s $SESSION
-# -------------------------------------------------------------------------------- 
-# Configure the Main window
-    
-    # Name the first window Main
-    tmux rename-window -t 1 'Main'
+    tmux new-session -d -s "$SESSION"
 
-    # Create Three window panes
+    # Name the first window Main
+    tmux rename-window -t "$SESSION:1" 'Main'
+
+    # Split the window into two horizontal panes
+    tmux split-window -v
+
     tmux selectp -t 1
-    tmux splitw -v -p 10
-    tmux selectp -t 1
-    tmux splitw -h -p 50
+    # Split the upper pane into two vertical panes
+    tmux split-window -h
+
+    tmux resize-pane -y 25
 # -------------------------------------------------------------------------------- 
 # Create second window for File Testing
     if [[ $language == "Python" ]] then
         cd ../tests
-		# Create second window with style matching the first 
+	# Create second window with style matching the first 
 		tmux new-window -t $SESSION:2 -n "Test"
-		tmux selectp -t 1
-		tmux splitw -v -p 10
-		tmux selectp -t 1
-		tmux splitw -h -p 50
+        tmux split-window -v
+        tmux selectp -t 1
+        # Split the upper pane into two vertical panes
+        tmux split-window -h
+        tmux resize-pane -y 25
 		cd ..
 	else
 		cd test
 		# Create second window with style matching the first 
 		tmux new-window -t $SESSION:2 -n "Test"
-		tmux selectp -t 1
-		tmux splitw -v -p 10
-		tmux selectp -t 1
-		tmux splitw -h -p 50
+        tmux split-window -v
+        tmux selectp -t 1
+        # Split the upper pane into two vertical panes
+        tmux split-window -h
+        tmux resize-pane -y 25
 		cd ../..
 	fi
 # -------------------------------------------------------------------------------- 
 # Create third window for README file 
 
     # Create second window with upper and lower window 
-	tmux new-window -t $SESSION:3 -n 'README'
+	tmux new-window -t $SESSION:3 -n 'README' -d 'nvim README.rst'
 	tmux send-keys -t $SESSION:3 'nvim README.rst' C-m
-
-    #tmux new-window -t $SESSION:3 -n 'README' -d 'nvim README.rst'
 # -------------------------------------------------------------------------------- 
 # Create fourth window for Python window 
 
@@ -110,7 +107,11 @@ if [ "$SESSIONEXISTS" = "" ]; then
 # Create fifth window for Python window 
    
     # Create second window with upper and lower window 
-    tmux new-window -t $SESSION:5 -n 'zsh'
+    if [[ $languate == "Python" ]] then
+        tmux new-window -t $SESSION:5 -n 'zsh'
+    else 
+        tmux new-window -t $SESSION:4 -n 'zsh'
+    fi
 # ================================================================================ 
 # Attach session 
 
